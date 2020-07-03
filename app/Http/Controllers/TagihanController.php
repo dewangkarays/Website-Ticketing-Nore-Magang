5c1c46e6a3f43cd73e4546d28891f30ce7d7499f
danielandii <danielandii@yahoo.com>
2020-07-03 21:20:34 +0700

    tagihan

---
+++ app/Http/Controllers/TagihanController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Model\Tagihan;
+use App\Model\User;
+
+class TagihanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tagihans = Tagihan::orderBy('id')->get();
+       
+        return view('tagihans.index', compact('tagihans'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $users = User::where('role','>=',80)->get();
+
+        return view('tagihans.create',compact('users'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            //
+        ]);
+
+        $data = $request->except(['_token', '_method']);
+        
+        $data['invoice'] = 'INV'.date('YmdHis');
+
+        $tagihan = Tagihan::create($data);
+        
+        return redirect('/tagihans')->with('success', 'Tagihan saved!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $users = User::where('role','>=',80)->get();
+        $tagihan = Tagihan::find($id);
+
+        return view('tagihans.edit', compact('tagihan','users')); 
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            //
+        ]);
+
+        $data = $request->except(['_token', '_method']);
+        $tagihan = Tagihan::find($id);
+
+        $tagihan->update($data);
+        
+        return redirect('/tagihans')->with('success', 'Tagihan updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $tagihan = Tagihan::find($id);
+        $tagihan->delete();
+
+        return redirect('/tagihans')->with('success', 'Tagihan deleted!');
+    }
+}
---
+++ database/migrations/2020_07_03_131553_create_tagihans_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagihansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tagihans', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->string('invoice');
+            $table->string('tipe');
+            $table->Integer('jml_tagih');
+            $table->Integer('jml_bayar')->default('0');
+            $table->tinyInteger('status')->default('0');
+            $table->string('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tagihans');
+    }
+}
