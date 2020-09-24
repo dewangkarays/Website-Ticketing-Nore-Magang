c935a53e876ef76ea28774dec1ee13a26c646362
nehemmiah <anggaharyokusumo@gmail.com>
2020-09-24 11:22:56 +0700

    tambahan nomor invoice di list lampiran

--- resources/views/tagihans/lampiran.blade.php
+++ resources/views/tagihans/lampiran.blade.php
@@ -14,7 +14,7 @@
 <div class="page-header page-header-light">
     <div class="page-header-content header-elements-md-inline">
         <div class="page-title d-flex">
-            <h4><i class="icon-arrow-left52 mr-2"></i> <span class="font-weight-semibold">List Lampiran</h4>
+            <h4><i class="icon-arrow-left52 mr-2"></i> <span class="font-weight-semibold">List Lampiran - {{$tagihan->invoice}} </h4>
                 <a href="#" class="header-elements-toggle text-default d-md-none"><i class="icon-more"></i></a>
             </div>
         </div>
