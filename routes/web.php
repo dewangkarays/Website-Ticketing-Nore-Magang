b2447e7eb15ad2b6001e6c2da026697a654322d7
Karta Kusuma <kartakusuma1199@gmail.com>
2022-08-25 13:28:17 +0700

    mengaktifkan routing getrekap

--- routes/web.php
+++ routes/web.php
@@ -126,7 +126,7 @@ Route::group(['middleware' => ['auth']], function() {
 		// Route::get('gettasks/{status}', 'TaskController@gettasks');
 		// Route::get('gettagihans', 'TagihanController@gettagihans');
 		Route::get('getrekapdp/{status}', 'RekapDptagihanController@getrekapdp');
-		// Route::get('getrekap/{status}', 'RekapTagihanController@getrekap');
+		Route::get('getrekap/{status}', 'RekapTagihanController@getrekap');
 		Route::get('getpayments', 'PaymentController@getpayments');
 		Route::get('getpengeluarans', 'PengeluaranController@getpengeluarans');
 	});
