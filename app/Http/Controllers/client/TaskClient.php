0cd1a8e8fc711ca2135ec1ae474519652121c24d
Karta Kusuma <kartakusuma1199@gmail.com>
2022-09-05 16:51:40 +0700

    mensinkronkan data task dari member

--- app/Http/Controllers/client/TaskClient.php
+++ app/Http/Controllers/client/TaskClient.php
@@ -137,8 +137,9 @@ class TaskClient extends Controller
     {
         //
         $tasks = Task::find($id);
-        $proyek = Proyek::find($tasks->id);
+        $proyek = Proyek::find($tasks->id_proyek);
         $proyek->task_count  = $proyek->task_count + 1;
+        $proyek->update();
         $tasks->delete();
         return redirect('/taskclient');
     }
