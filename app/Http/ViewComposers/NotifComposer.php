7e050b6d3fe863611014f39be1574d3e5664abbf
Karta Kusuma <kartakusuma1199@gmail.com>
2022-09-08 14:59:21 +0700

    menambahkan comfirmpayments

--- app/Http/ViewComposers/NotifComposer.php
+++ app/Http/ViewComposers/NotifComposer.php
@@ -5,6 +5,7 @@ namespace App\Http\ViewComposers;
 use Illuminate\Contracts\View\View;
 use App\Model\User;
 use App\Model\Tagihan;
+use App\Model\Payment;
 use Carbon\Carbon;
 
 class NotifComposer
@@ -15,6 +16,7 @@ class NotifComposer
         $expired = User::where('kadaluarsa', '<', Carbon::now()->toDateString())->get()->count();
         $admunpaid = Tagihan::where('status','!=',2)->count();
         $userunpaid = Tagihan::where('status','!=',2)->where('user_id',\Auth::user()->id)->count();
-        $view->with(compact('expired','admunpaid','userunpaid'));
+        $confirmpayments = Payment::where('status', '=', 0)->count();
+        $view->with(compact('expired','admunpaid','userunpaid','confirmpayments'));
     }
 }
