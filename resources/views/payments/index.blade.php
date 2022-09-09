e10f0b38ec0f8bb766f28091aae905fdab7b29a1
Karta Kusuma <kartakusuma1199@gmail.com>
2022-09-09 11:13:25 +0700

    memperbarui format number

--- resources/views/payments/index.blade.php
+++ resources/views/payments/index.blade.php
@@ -231,30 +231,6 @@
 			'2': 'Ditolak',
 		};
 
-		function number_format (number, decimals, dec_point, thousands_sep) {
-			// Strip all characters but numerical ones.
-			number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
-			var n = !isFinite(+number) ? 0 : +number,
-				prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
-				sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
-				dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
-				s = '',
-				toFixedFix = function (n, prec) {
-					var k = Math.pow(10, prec);
-					return '' + Math.round(n * k) / k;
-				};
-			// Fix for IE parseFloat(0.55).toFixed(0) = 0;
-			s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
-			if (s[0].length > 3) {
-				s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
-			}
-			if ((s[1] || '').length < prec) {
-				s[1] = s[1] || '';
-				s[1] += new Array(prec - s[1].length + 1).join('0');
-			}
-			return s.join(dec);
-		};
-
 		// Basic Datatable examples
 		var _componentDatatableBasic = function() {
 			if (!$().DataTable) {
@@ -310,7 +286,7 @@
 						data: null,
 						name: null,
 						render: (data, type, row) => {
-							return number_format(data?.nominal, 0, ',', ',');
+							return number_format(data?.nominal, 0, '.', '.');
 						}
 					},
 					{
