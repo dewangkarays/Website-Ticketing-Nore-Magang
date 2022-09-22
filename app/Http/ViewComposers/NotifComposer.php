77816a509a20a3d99a2c04ecb436ec114d27a78c
Karta Kusuma <kartakusuma1199@gmail.com>
2022-09-23 04:42:18 +0700

    menambahkan notifikasi untuk verifikasi cuti

--- app/Http/ViewComposers/NotifComposer.php
+++ app/Http/ViewComposers/NotifComposer.php
@@ -6,17 +6,48 @@ use Illuminate\Contracts\View\View;
 use App\Model\User;
 use App\Model\Tagihan;
 use App\Model\Payment;
+use App\Model\Cuti;
 use Carbon\Carbon;
+use Auth;
 
 class NotifComposer
 {
 
     public function compose(View $view)
     {
+        $currentUserId = Auth::id();
+
         $expired = User::where('kadaluarsa', '<', Carbon::now()->toDateString())->get()->count();
         $admunpaid = Tagihan::where('status','!=',2)->count();
         $userunpaid = Tagihan::where('status','!=',2)->where('user_id',\Auth::user()->id)->count();
         $confirmpayments = Payment::where('status', '=', 0)->count();
-        $view->with(compact('expired','admunpaid','userunpaid','confirmpayments'));
+        $unverifiedcutiv = Cuti::where(
+            function($q) use ($currentUserId) {
+                $q->where(
+                    function($q1) use ($currentUserId) {
+                        $q1->where('verifikator_2_id', $currentUserId)
+                            ->where('verifikasi_2', '1');
+                    }
+                )
+                ->orWhere(
+                    function($q2) use ($currentUserId) {
+                        $q2->where('verifikator_1_id', $currentUserId)
+                            ->where('verifikasi_2', '2')
+                            ->where('verifikasi_1', '1');
+                    }
+                );
+            }
+        )
+        ->where('status', '<', '3')
+        ->count();
+        $unverifiedcutis = Cuti::where(
+            function($q) {
+                $q->where('verifikasi_2', '<', '2')
+                    ->orWhere('verifikasi_1', '<', '2');
+            }
+        )
+        ->where('status', '<', '3')
+        ->count();
+        $view->with(compact('expired','admunpaid','userunpaid','confirmpayments','unverifiedcutiv','unverifiedcutis'));
     }
 }
