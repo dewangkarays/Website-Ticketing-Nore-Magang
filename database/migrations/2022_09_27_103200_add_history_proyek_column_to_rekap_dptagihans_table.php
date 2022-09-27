1949092b4418da9016c04b65448c2db114e83edd
Karta Kusuma <kartakusuma1199@gmail.com>
2022-09-27 11:15:23 +0700

    membuat kolom history_proyek

---
+++ database/migrations/2022_09_27_103200_add_history_proyek_column_to_rekap_dptagihans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddHistoryProyekColumnToRekapDptagihansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rekap_dptagihans', function (Blueprint $table) {
+            $table->text('history_proyek')->nullable()->after('keterangan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('rekap_dptagihans', function (Blueprint $table) {
+            $table->dropColumn('history_proyek');
+        });
+    }
+}
