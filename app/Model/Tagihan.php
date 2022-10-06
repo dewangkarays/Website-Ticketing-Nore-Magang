6ea415e532500087328a1dd77b7c0799f9622f83
Karta Kusuma <kartakusuma1199@gmail.com>
2022-10-06 16:45:54 +0700

    menambahkan fitur soft deletes pada model Tagihan

--- app/Model/Tagihan.php
+++ app/Model/Tagihan.php
@@ -3,9 +3,12 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Tagihan extends Model
 {
+    use SoftDeletes;
+    
     protected $guarded = [
         'id', 'created_at'
     ];
