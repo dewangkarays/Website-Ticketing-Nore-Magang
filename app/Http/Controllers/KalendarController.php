1c76e924da515ce1d79940afd6a4aa6eaac9c390
Karta Kusuma <kartakusuma1199@gmail.com>
2022-10-18 14:15:50 +0700

    menambahkan cuti ke kalendar

--- app/Http/Controllers/KalendarController.php
+++ app/Http/Controllers/KalendarController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Model\Presensi;
+use App\Model\Cuti;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Collection;
 
 class KalendarController extends Controller
 {
@@ -15,11 +17,26 @@ class KalendarController extends Controller
         $presensi = Presensi::where('status', '!=', '1')->get();
 
         foreach ($presensi as $item) {
+            $item['tanggal_mulai'] = $item->tanggal;
+            $item['tanggal_akhir'] = $item->tanggal;
             $item['nama'] = $item->karyawan->nama;
         }
 
+        $cuti = Cuti::where('status', '2')
+            ->where('tanggal_mulai', '<=', date('Y-m-d'))
+            ->where('tanggal_akhir', '>=', date('Y-m-d'))
+            ->get();
+
+        foreach ($cuti as $dataCuti) {
+            $dataCuti['nama'] = $dataCuti->karyawan->nama;
+        }
+
+        $dataKalendar = new Collection;
+        $dataKalendar = $dataKalendar->merge($presensi);
+        $dataKalendar = $dataKalendar->merge($cuti);
+
         // dd($presensi);
 
-        return response()->json($presensi);
+        return response()->json($dataKalendar);
     }
 }
