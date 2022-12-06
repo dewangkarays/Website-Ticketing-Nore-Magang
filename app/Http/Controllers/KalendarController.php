0c0b33670d0df54965cacee31611baa8c0057450
farizzak <faathirizak00@gmail.com>
2022-12-06 15:43:25 +0700

    ajax eror viewkalender

--- app/Http/Controllers/KalendarController.php
+++ app/Http/Controllers/KalendarController.php
@@ -39,7 +39,7 @@ class KalendarController extends Controller
         $cuti = Cuti::where('status', '2')->get();
 
         foreach ($cuti as $dataCuti) {
-            $dataCuti['nama'] = $dataCuti->karyawan->nama;
+            $dataCuti['nama'] = @$dataCuti->karyawan->nama;
             $dataCuti['url'] = '/cuti/'.$dataCuti->id;
             $dataCuti->tanggal_akhir = date('Y-m-d', strtotime($dataCuti->tanggal_akhir.'+1 days'));
             $dataCuti['warna'] = '#7e8082'; //Grey
