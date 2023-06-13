7c0f9c5327aea33c564c8c2fe2b8335462bc15c3
farizzak <faathirizak00@gmail.com>
2023-06-13 13:57:44 +0700

    favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
+	<link href="{{asset('global_assets/images/Nore_Iklan_Nore.png') }}" rel="shortcut icon" type="image/x-icon">
 	<title>Nore - Pengoperasian Website</title>
 
 	<!-- Global stylesheets -->
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link href="{{asset('global_assets/images/Nore_Iklan_Nore.png') }}" rel="shortcut icon" type="image/x-icon">
 	<title>OP Ticketing</title>
 
 	<!-- Global stylesheets -->
