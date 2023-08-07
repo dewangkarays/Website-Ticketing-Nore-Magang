91cd1cdcb92a0044aa6abe6f7dc7bcdcfd2eb63a
danielandii <danielandii@yahoo.com>
2023-08-07 14:45:53 +0700

    tambah route test

--- routes/api.php
+++ routes/api.php
@@ -44,3 +44,8 @@ Route::group(['middleware' => ['auth:api']], function() {
 
     Route::get('/logout',  'Api\LoginApiController@logout');
 });
+
+//routebaru
+    Route::get('/testroute',  'Api\LoginApiController@test');
+
+
