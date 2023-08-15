1540efa0f5a65a714c630a2a502872adf7a516e0
djamarlianad <djamar.lianad@gmail.com>
2023-08-15 12:22:30 +0700

    update kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,6 +20,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\Cors::class,
 
         // appending custom middleware 
         \App\Http\Middleware\HttpsProtocol::class,
@@ -39,7 +40,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\Cors::class
+            
         ],
 
         'api' => [
